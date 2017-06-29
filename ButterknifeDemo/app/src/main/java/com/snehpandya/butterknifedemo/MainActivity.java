4a2eafd344ed8e3b81a2c83168d586f3ce79840a
Sneh Pandya <sneh.pandya1@gmail.com>
2017-06-29 11:34:03 +0530

    Demo App using ButterKnife library

---
+++ ButterknifeDemo/app/src/main/java/com/snehpandya/butterknifedemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.snehpandya.butterknifedemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.name)
+    TextView title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        title.setText("Sneh Pandya");
+    }
+}
