dc1607515852342088fbe17d0c9d5f697c7ebeee
Sneh Pandya <sneh.pandya1@gmail.com>
2017-07-06 15:06:18 +0530

    Demo App using ConstraintLayout

---
+++ ConstraintLayoutDemo/app/src/main/java/com/snehpandya/constraintlayoutdemo/ChainActivity.java
@@ -0,0 +1,17 @@
+package com.snehpandya.constraintlayoutdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sneh.pandya on 06/07/17.
+ */
+
+public class ChainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chain);
+    }
+}
---
+++ ConstraintLayoutDemo/app/src/main/java/com/snehpandya/constraintlayoutdemo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.snehpandya.constraintlayoutdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+//https://slideslive.com/38900612/cool-constraintlayout
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ChainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
